7f2e3a718b7a9e09b9ad4b0ad01661ecf5fd310a
Angelo Gaona <ang.dev@hotmail.com>
2025-03-22 02:34:48 -0500

    domain front in Cors test

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: 'https://ufo-trade-front.onrender.com',
         credentials: true,
     })
 );
